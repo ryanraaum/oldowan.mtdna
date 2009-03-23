befdef0534b72f4b996d12186175d2670894909d
Ryan Raaum <code@raaum.org>
2009-03-22 20:58:10 -0400

    add more to import list

--- oldowan/mtdna/__init__.py
+++ oldowan/mtdna/__init__.py
@@ -15,8 +15,20 @@ try:
     from oldowan.mtdna.rCRS import rCRSlist
     from oldowan.mtdna.rCRS import rCRSplus
     from oldowan.mtdna.rCRS import rCRSplus_positions
+    from oldowan.mtdna.rCRS import HVR1_indices
+    from oldowan.mtdna.rCRS import HVR2_indices
+    from oldowan.mtdna.rCRS import HVR1and2_indices
+    from oldowan.mtdna.rCRS import HVR1to2_indices
+    from oldowan.mtdna.rCRS import coding_indices
+    from oldowan.mtdna.rCRS import all_indices
 except:
     from rCRS import rCRS
     from rCRS import rCRSlist
     from rCRS import rCRSplus
     from rCRS import rCRSplus_positions
+    from rCRS import HVR1_indices
+    from rCRS import HVR2_indices
+    from rCRS import HVR1and2_indices
+    from rCRS import HVR1to2_indices
+    from rCRS import coding_indices
+    from rCRS import all_indices
